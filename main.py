6c230cdb41a7570063ee6315df73c508c7329497
Alice maganhoto <alice.maganhoto@sou.unifeob.edu.br>
2026-05-06 22:20:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from view.FilmeView import FilmeView
+from view.SessaoView import SessaoView
 
+print("=== CADASTRO DE FILME ===")
+
+filmeView = FilmeView()
+filmeView.cadastrarFilme()
+
+print("\n=== REGISTRO DE PUBLICO ===")
+
+sessaoView = SessaoView()
+sessaoView.registrarPublico()
